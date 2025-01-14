9b573cbfd691553bb2d808465954a4a1c3b19c0b
khurramjaved96 <mail@khurramjaved.com>
2025-01-14 15:04:43 -0500

    Add call for workshops

--- data.js
+++ data.js
@@ -67,7 +67,7 @@ listOfMenuItems = [
     // new MenuItem("Organizers", "#"),
 
     // new MenuItemWithSubmenu("Year", [ new MenuItem("2024", "2024/index.html"), new MenuItem("2025", "index.html")] ),
-    new MenuItemWithSubmenu("Submit", [new MenuItem("Call&nbspfor&nbspPapers", "callforpapers.html"),  new MenuItem("Submission&nbspInstructions", "submissionInstructions.html")] ),
+    new MenuItemWithSubmenu("Submit", [new MenuItem("Call&nbspfor&nbspPapers", "callforpapers.html"), new MenuItem("Call&nbspfor&nbspWorkshops", "callforworkshops.html"), new MenuItem("Submission&nbspInstructions", "submissionInstructions.html")] ),
     new MenuItem("Organizers", "organizers.html"),
     new MenuItemWithSubmenu("Year", [new MenuItem("2024", "/2024/index.html"),  new MenuItem("2025", "index.html")] ),
     new MenuItem("Contact&nbspUs", "contact.html"),
