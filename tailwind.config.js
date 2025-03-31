10a4591df28a2901255c7ac638f2c83e08c7e2d2
Khurram Javed <mail@khurramjaved.com>
2025-03-31 09:59:21 -0400

    Add registration confirmation page

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,6 +1,6 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-  content: ["index.html", "guidelines.html", "organizers.html", "reviewProcess.html", "callforpapers.html", "recordings.html", "submissionInstructions.html", "contact.html", "advisors.html", "reviewers.html", "sponsors.html", "reviewinstructions.html", "participate.html", "register.html", "hotels.html", "hotelmap.html"],
+  content: ["index.html", "guidelines.html", "organizers.html", "reviewProcess.html", "callforpapers.html", "recordings.html", "submissionInstructions.html", "contact.html", "advisors.html", "reviewers.html", "sponsors.html", "reviewinstructions.html", "participate.html", "register.html", "hotels.html", "hotelmap.html", "registration_confirmation.html"],
   theme: {
     colors: {
       'rlyellow': {
