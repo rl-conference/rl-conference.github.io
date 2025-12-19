63cd21e99e86d163b6d9c280cd569a561b541b3b
Khurram Javed <mail@khurramjaved.com>
2025-12-19 14:58:55 -0500

    Add call for papers

--- data.js
+++ data.js
@@ -71,16 +71,18 @@ class MenuItemWithSubmenu {
 
 
 listOfMenuItems = [
+    new MenuItemWithSubmenu("Submit", [
+        new MenuItem("Call&nbsp;for&nbsp;Papers", "callforpapers.html"), new MenuItem("Submission&nbsp;Instructions", "submissionInstructions.html")]),
     new MenuItemWithSubmenu("Organizers", [new MenuItem("Organizers", "organizers.html"), new MenuItem("Advisors", "advisors.html"), new MenuItem("Board&nbsp;Members", "boardmembers.html")]),
     new MenuItemWithSubmenu("Previous Years", [new MenuItem("2025", "/2025/index.html"), new MenuItem("2024", "/2024/index.html")]),
 ]
 
 
 ///// Footer /////
-let footerText = `© 2025 Reinforcement Learning Conference Organization Committee`
+let footerText = `© 2026 Reinforcement Learning Conference Organization Committee`
 
 //// SEO ////
-let mainPageTitleforSEO = 'RLC 2025'
+let mainPageTitleforSEO = 'Reinforcement Learning Conference'
 let homeSearchEngineDescription = "Reinforcement Learning Conference."
 
 
